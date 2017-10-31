38681536538f0b557d0bab91e0a35ee3627703a9
Ortix92 <nicktsutsunava@gmail.com>
2017-10-31 23:09:27 +0100

    rainbow implemented and working

--- visualed.js
+++ visualed.js
@@ -13,6 +13,28 @@ class Visualed {
         this.setStripColor(...this.color).delay(delay).then(() => { this.run() })
     }
 
+    rainbowSingle(delay, f, i = 0) {
+        // Delay is in ms so we divide by 1000
+        let sample = i * (delay / 1000)
+        if (sample >= 1 / f) {
+            i = 0
+        }
+        // We let the sine wave become negative since it's interpreted as 0
+        // This saves us a couple of if statements ;)
+        let r = Math.floor((Math.sin(sample * 2 * Math.PI * f)) * 127)
+        let g = Math.floor((Math.sin(sample * 2 * Math.PI * f + 2 * Math.PI / 3)) * 127)
+        let b = Math.floor((Math.sin(sample * 2 * Math.PI * f + 4 * Math.PI / 3)) * 127)
+        this.color = [r, g, b]
+        // console.log(`[Sample: ${sample}] [i: ${i}] [r: ${r}]`)
+        // console.log(sample)
+        return Promise.resolve().delay(delay).then(() => {
+            i++
+            this.rainbowSingle(delay, f, i)
+        })
+    }
+
+
+
     setStripColor(r, g, b) {
         let params = device.interpretParameters(r, g, b)
         let arr = new Array(this.activeLeds).fill([params.green, params.red, params.blue])
@@ -26,7 +48,6 @@ class Visualed {
     increase(delay) {
         if (this.activeLeds < this.MAX_LEDS) {
             this.activeLeds++
-            console.log(this.activeLeds)
             return Promise.delay(delay).then(() => // implicit return from concise arrow body
                 this.increase(delay)
             )
@@ -38,7 +59,6 @@ class Visualed {
     decrease(delay) {
         if (this.activeLeds > 0) {
             this.activeLeds--
-            console.log(this.activeLeds)
             return Promise.delay(delay).then(() => // implicit return from concise arrow body
                 this.decrease(delay)
             )
@@ -47,14 +67,19 @@ class Visualed {
         }
     }
 
-
     oscillate(delay, increase = true) {
         return this.increase(delay).then(() => this.decrease(delay)).then(() => this.oscillate(delay))
     }
 
+    on() {
+        this.activeLeds = this.MAX_LEDS
+        return Promise.resolve()
+    }
+
     off() {
-        let arr = new Array(this.MAX_LEDS*3).fill(0)
+        let arr = new Array(this.MAX_LEDS * 3).fill(0)
         device.setColors(0, arr)
+        return Promise.resolve()
     }
 
 }
@@ -64,7 +89,9 @@ module.export = new Visualed
 let app = new Visualed
 
 app.run(10)
-app.oscillate(10)
+app.rainbowSingle(10, 0.1)
+// app.oscillate(10)
+app.on()
 
 process.on('SIGINT', function () {
     console.log("Caught interrupt signal");
