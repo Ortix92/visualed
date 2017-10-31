85c182b69730f718ceb81263132de7918f6acae3
Ortix92 <nicktsutsunava@gmail.com>
2017-10-31 21:48:36 +0100

    LEDs also working now

--- visualed.js
+++ visualed.js
@@ -1,13 +1,27 @@
+var blinkstick = require("blinkstick")
 let Promise = require("bluebird")
-
+const device = Promise.promisifyAll(blinkstick.findFirst())
 class Visualed {
 
     constructor() {
         this.MAX_LEDS = 56
         this.activeLeds = 1
-        this.color = [0, 0, 0]
+        this.color = [255, 0, 0]
     }
 
+    run(delay) {
+        this.setStripColor(...this.color).delay(delay).then(() => { this.run() })
+    }
+
+    setStripColor(r, g, b) {
+        let params = device.interpretParameters(r, g, b)
+        let arr = new Array(this.activeLeds).fill([params.green, params.red, params.blue])
+        let flat = [].concat.apply([], arr)
+        device.setColors(0, flat)
+
+        return Promise.resolve()
+
+    }
 
     increase(delay) {
         if (this.activeLeds < this.MAX_LEDS) {
@@ -38,10 +52,22 @@ class Visualed {
         return this.increase(delay).then(() => this.decrease(delay)).then(() => this.oscillate(delay))
     }
 
+    off() {
+        let arr = new Array(this.MAX_LEDS*3).fill(0)
+        device.setColors(0, arr)
+    }
+
 }
 
 module.export = new Visualed
 
 let app = new Visualed
 
-app.bounce(50)
+app.run(10)
+app.oscillate(10)
+
+process.on('SIGINT', function () {
+    console.log("Caught interrupt signal");
+    app.off()
+    process.exit();
+})
